e00e53b51e366c2108ba3a41dccd6ea0fb48d08a
Elizaveta Prokofeva <prokof.15@mail.ru>
2021-05-01 18:11:03 +0300

    first

---
+++ modules/task_3/prokofeva_e_sparse_crs_tbb/sparse_crs.h
@@ -0,0 +1,20 @@
+// Copyright 2021 Prokofeva Elizaveta
+#ifndef MODULES_TASK_3_PROKOFEVA_E_SPARSE_CRS_TBB_SPARSE_CRS_H_
+#define MODULES_TASK_3_PROKOFEVA_E_SPARSE_CRS_TBB_SPARSE_CRS_H_
+
+#include<vector>
+
+using std::vector;
+
+struct crs_matrix {
+    int N;
+    vector<double> values;
+    vector<int> cols;
+    vector<int> row_index;
+};
+crs_matrix transpose(crs_matrix matrix);
+crs_matrix mult(crs_matrix first, crs_matrix second);
+crs_matrix generate(int size);
+crs_matrix mult_tbb(crs_matrix first, crs_matrix second);
+
+#endif  // MODULES_TASK_3_PROKOFEVA_E_SPARSE_CRS_TBB_SPARSE_CRS_H_
