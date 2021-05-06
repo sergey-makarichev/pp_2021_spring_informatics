1e035df34328ea18751bd5683f5267459c72ceb2
MG_PC <vlad-hk.19.03@mail.ru>
2021-05-06 16:26:10 +0300

    fix_test_time

--- modules/task_2/bulychev_v_int_rec/main.cpp
+++ modules/task_2/bulychev_v_int_rec/main.cpp
@@ -4,36 +4,64 @@
 #include <vector>
 #include "./int_rec_omp.h"
 
-double f2(double x, double y) {
+double f(std::vector<double> t) {
+    double x = t[0];
+    double y = t[1];
+    return x * x - y * y;
+}
+
+double f5(std::vector<double> t) {
+    double x = t[0];
+    double y = t[1];
+    return x * x + y * y;
+}
+
+double f3(std::vector<double> t) {
+    double x = t[0];
+    double y = t[1];
     return x + y + 1;
 }
 
-double f1(double x, double y) {
-    return x * x * x - y * y;
+double f2(std::vector<double> t) {
+    double x = t[0];
+    return x * x - x;
 }
 
-TEST(Integrate_rectangle, DISABLED_Test_int_rec_1) {
+double f1(std::vector<double> t) {
+    double x = t[0];
+    double y = t[1];
+    double z = t[2];
+    return (z - (5 * x + 2 * y));
+}
+
+double f4(std::vector<double> t) {
+    double x = t[0];
+    double y = t[1];
+    return x * 10 - x * y + cos(y);
+}
+
+TEST(Integrate_rectangle, Test_int_rec_1) {
     int s = 2;
     std::vector<double> a(s);
     std::vector<double> b(s);
 
-    a[0] = 5;
-    b[0] = 17;
-    a[1] = 21;
-    b[1] = 53;
+    a[0] = 0;
+    b[0] = 3;
+    a[1] = 0;
+    b[1] = 3;
 
     double t1 = omp_get_wtime();
-    double ans1 = Calculation_Seq(a, b, 1000, f1);
+    double ans1 = Calculation_Seq(a, b, 100, f5);
     double t2 = omp_get_wtime();
     std::cout << "1: " << ans1 << std::endl;
     std::cout << "Seq: " << t2 - t1 << std::endl;
     t1 = omp_get_wtime();
-    double ans2 = Calculation_Omp(a, b, 1000, f1);
+    double ans2 = Calculation_Omp(a, b, 100, f5);
     t2 = omp_get_wtime();
     std::cout << "2: " << ans2 << std::endl;
     std::cout << "Omp: " << t2 - t1 << std::endl;
 
-    ASSERT_NEAR(ans2, ans1, 5000);
+    ASSERT_NEAR(ans1, ans2, 100);
 }
 
 TEST(Integrate_rectangle, Test_int_rec_2) {
@@ -41,25 +69,31 @@ TEST(Integrate_rectangle, Test_int_rec_2) {
     std::vector<double> a(s);
     std::vector<double> b(s);
 
-    a[0] = 5;
-    b[0] = 14;
-    a[1] = 3;
-    b[1] = 21;
+    a[0] = 0;
+    b[0] = 5;
+    a[1] = 0;
+    b[1] = 3;
 
-    ASSERT_NO_THROW(Calculation_Omp(a, b, 5, f2));
+    double ans = 79;
+
+    double ans1 = Calculation_Omp(a, b, 10, f);
+
+    ASSERT_NEAR(ans, ans1, 100);
 }
 
 TEST(Integrate_rectangle, Test_int_rec_3) {
-    int s = 2;
+    int s = 1;
     std::vector<double> a(s);
     std::vector<double> b(s);
 
     a[0] = 5;
-    b[0] = 14;
-    a[1] = 3;
-    b[1] = 21;
+    b[0] = 25;
 
-    ASSERT_NO_THROW(Calculation_Seq(a, b, 5, f2));
+    double ans = 4867;
+
+    double ans1 = Calculation_Omp(a, b, 100, f2);
+
+    ASSERT_NEAR(ans, ans1, 1000);
 }
 
 TEST(Integrate_rectangle, Test_int_rec_4) {
@@ -67,32 +101,31 @@ TEST(Integrate_rectangle, Test_int_rec_4) {
     std::vector<double> a(s);
     std::vector<double> b(s);
 
-    a[0] = 5;
-    b[0] = 14;
-    a[1] = 3;
-    b[1] = 21;
+    a[0] = 0;
+    b[0] = 1;
+    a[1] = 2;
+    b[1] = 4;
 
-    double ans1 = Calculation_Seq(a, b, 50, f1);
-    double ans2 = Calculation_Omp(a, b, 50, f1);
+    double ans = 9;
 
-    ASSERT_NEAR(ans2, ans1, 10000);
+    double ans1 = Calculation_Omp(a, b, 10, f3);
+
+    ASSERT_NEAR(ans, ans1, 10);
 }
 
 TEST(Integrate_rectangle, Test_int_rec_5) {
-    int s = 2;
+    int s = 3;
     std::vector<double> a(s);
     std::vector<double> b(s);
 
-    a[0] = 5;
-    b[0] = 17;
-    a[1] = 21;
-    b[1] = 53;
+    a[0] = 4;
+    b[0] = 10;
+    a[1] = 7;
+    b[1] = 33;
+    a[2] = -1;
+    b[2] = 5;
 
-    double ans = 104704;
-
-    double ans1 = Calculation_Seq(a, b, 100, f1);
-
-    ASSERT_NEAR(ans1, ans, 50000);
+    ASSERT_NO_THROW(Calculation_Omp(a, b, 5, f1));
 }
 
 TEST(Integrate_rectangle, Test_int_rec_6) {
@@ -100,16 +133,12 @@ TEST(Integrate_rectangle, Test_int_rec_6) {
     std::vector<double> a(s);
     std::vector<double> b(s);
 
-    a[0] = 5;
-    b[0] = 17;
-    a[1] = 21;
-    b[1] = 53;
+    a[0] = 4;
+    b[0] = 10;
+    a[1] = 7;
+    b[1] = 33;
 
-    double ans = 104704;
-
-    double ans1 = Calculation_Omp(a, b, 100, f1);
-
-    ASSERT_NEAR(ans1, ans, 50000);
+    ASSERT_NO_THROW(Calculation_Omp(a, b, 5, f4));
 }
 
 int main(int argc, char **argv) {
