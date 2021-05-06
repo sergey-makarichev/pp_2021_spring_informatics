fd2c6800c5fe19946f92514df1d8d675a844337b
ChirkovRoman <romul14603@list.ru>
2021-05-06 17:41:16 +0300

    changed test group name

--- modules/task_3/chirkov_r_jarvis_march/main.cpp
+++ modules/task_3/chirkov_r_jarvis_march/main.cpp
@@ -6,7 +6,7 @@
 #include "./jarvis_march.h"
 #include "tbb/tick_count.h"
 
-TEST(jarvis_march_OMP, jarvis_bunch_of_points) {
+TEST(jarvis_march_TBB, jarvis_bunch_of_points) {
   std::vector<Point> points = {Point(1, 1), Point(0, 0),    Point(10, 0),
                                Point(0, 6), Point(14, 200), Point(230, 15),
                                Point(4, 2)};
@@ -15,33 +15,33 @@ TEST(jarvis_march_OMP, jarvis_bunch_of_points) {
 
   ASSERT_EQ(exp, res);
 }
-TEST(jarvis_march_OMP, generation_wrong_size) {
+TEST(jarvis_march_TBB, generation_wrong_size) {
   std::vector<Point> points;
   ASSERT_ANY_THROW(points = makePointsArray(0));
 }
 
-TEST(jarvis_march_OMP, jarvis_one_point) {
+TEST(jarvis_march_TBB, jarvis_one_point) {
   std::vector<Point> points = {Point(123, 321)};
   std::vector<int> exp = {0, 0};
   std::vector<int> res = ParallelJarvis(points);
   ASSERT_EQ(exp, res);
 }
 
-TEST(jarvis_march_OMP, jarvis_two_points) {
+TEST(jarvis_march_TBB, jarvis_two_points) {
   std::vector<Point> points = {Point(123, 321), Point(1, 2)};
   std::vector<int> exp = {1, 0, 1};
   std::vector<int> res = ParallelJarvis(points);
   ASSERT_EQ(exp, res);
 }
 
-TEST(jarvis_march_OMP, jarvis_two_bottom_points) {
+TEST(jarvis_march_TBB, jarvis_two_bottom_points) {
   std::vector<Point> points = {Point(123, 321), Point(1, 2), Point(4, 2)};
   std::vector<int> exp = {1, 2, 0, 1};
   std::vector<int> res = ParallelJarvis(points);
   ASSERT_EQ(exp, res);
 }
 
-TEST(jarvis_march_OMP, jarvis_three_points_on_the_same_line) {
+TEST(jarvis_march_TBB, jarvis_three_points_on_the_same_line) {
   std::vector<Point> points = {Point(400, 400), Point(300, 400),
                                Point(100, 400)};
   std::vector<int> exp = {2, 0, 2};
@@ -49,18 +49,18 @@ TEST(jarvis_march_OMP, jarvis_three_points_on_the_same_line) {
   ASSERT_EQ(exp, res);
 }
 
-TEST(jarvis_march_OMP, generation_and_Jarvis) {
+TEST(jarvis_march_TBB, generation_and_Jarvis) {
   std::vector<Point> points = makePointsArray(5000);
   ASSERT_NO_THROW(ParallelJarvis(points));
 }
 
-TEST(jarvis_march_OMP, generationCircle_and_Jarvis) {
+TEST(jarvis_march_TBB, generationCircle_and_Jarvis) {
   std::vector<Point> points = makePointsArrayCircle(5000);
   SequentialJarvis(points);
   ASSERT_NO_THROW(ParallelJarvis(points));
 }
 
-TEST(jarvis_march_OMP, seq_and_par_compare) {
+TEST(jarvis_march_TBB, seq_and_par_compare) {
   std::vector<Point> points = makePointsArray(100000);
   tbb::tick_count start, stop;
   std::vector<int> seqHull, parHull;
@@ -78,7 +78,7 @@ TEST(jarvis_march_OMP, seq_and_par_compare) {
   ASSERT_EQ(parHull, seqHull);
 }
 
-TEST(jarvis_march_OMP, circle_seq_and_par_compare) {
+TEST(jarvis_march_TBB, circle_seq_and_par_compare) {
   std::vector<Point> points = makePointsArrayCircle(100000);
   tbb::tick_count start, stop;
   std::vector<int> seqHull, parHull;
