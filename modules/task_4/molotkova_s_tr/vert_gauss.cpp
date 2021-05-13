d537aeb8047066b9fdddc07294bb982d8ce23531
SMolotkova <svetlanamolotkova2000@gmail.com>
2021-05-13 11:50:40 +0300

    2

--- modules/task_4/molotkova_s_tr/vert_gauss.cpp
+++ modules/task_4/molotkova_s_tr/vert_gauss.cpp
@@ -91,7 +91,7 @@ std::vector<double> gaussFilter_par(const std::vector<double>& matrix, int rows,
   int offset = rows / nthreads;
   int rem = rows % nthreads;
   for (int i = 0; i < nthreads; i++) {
-        threads[i] = std::thread([i, offset, nthreads, rem, cols, radius, size, rows,
+        threads[i] = std::thread([offset, nthreads, rem, cols, radius, size, rows,
         &kernel, &matrix, &resultMatrix](int thread) {
           int begin = thread * offset;
           int numrows;
